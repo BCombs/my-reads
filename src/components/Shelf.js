7a465debac0882e3b6da5397164629e3a95e9fd2
BCombs <bill.c.combs@outlook.com>
2019-04-16 20:36:43 +0000

    Added Shelf component

---
+++ src/components/Shelf.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Shelf extends Component {
+  
+  
+  render() {
+    const shelfName = this.props.name;
+    
+    return (
+      <div className="bookshelf">
+      	<h2 className="bookshelf-title">{shelfName}</h2>
+      	<div className="bookshelf-books">
+      		<ol className="books-grid">
+      		</ol>
+      	</div>
+      </div>
+    );
+  }
+}
+
+export default Shelf;
+
+Shelf.propTypes = {
+  name: PropTypes.string.isRequired
+}
